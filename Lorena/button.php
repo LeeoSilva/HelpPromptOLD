751e0dd195c76e704406d8e81fd691e65b275e5e
LeeoSilva <leonardo.silva1199@etec.sp.gov.br>
2017-12-12 20:23:51 -0200

    /Lorena removido

--- Lorena/button.php
+++
@@ -1,5 +0,0 @@
-<div class="fixed-action-btn col s12 m6 l6 subscribe-me" id="button">
-	<a class="btn-floating btn-large teal" href="index.php" >
-		<i class="large material-icons">arrow_upward</i>
-	</a>
-</div>
