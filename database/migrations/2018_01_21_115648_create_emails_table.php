60083328a2ba2795a2a3aa23d43582b98354323f
LeeoSilva <leonardo.silva1199@etec.sp.gov.br>
2018-02-03 16:17:24 -0200

    Email empresarial adicionado ao banco

---
+++ database/migrations/2018_01_21_115648_create_emails_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emails', function (Blueprint $table) {
+					$table->string('usr_mail', 191)->primary();
+					$table->string('usr_name');
+					$table->foreign('usr_name')->references('usr_name')->on('logins')->onDelete('CASCADE');
+					$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emails');
+    }
+}
--- database/migrations/2018_01_21_115713_create_empresas_table.php
+++ database/migrations/2018_01_21_115713_create_empresas_table.php
@@ -15,7 +15,8 @@ class CreateEmpresasTable extends Migration
     {
         Schema::create('empresas', function (Blueprint $table) {
 					$table->increments('empresa_id')->unsigned();
-					$table->string('empresa_nome');
+					$table->string('empresa_nome', 191);
+          $table->string('estab_email', 191);
 					$table->integer('estabelecimentos')->unsigned();
 					$table->string('tipo_servico', 191);
 					$table->integer('telefones')->unsigned();
@@ -26,15 +27,17 @@ class CreateEmpresasTable extends Migration
 					$table->string('endereco', 191);
 					$table->integer('veiculos')->unsigned();
 					$table->integer('cep')->unsigned();
-					$table->foreign('estabelecimentos')->references('estab_id')->on('estabelecimentos')->onDelete('CASCADE');
-					$table->foreign('tipo_servico')->references('tipo_servico')->on('tipo_servicos')->onDelete('CASCADE');
-					$table->foreign('telefones')->references('estab_tel')->on('telefones')->onDelete('CASCADE');
-					$table->foreign('representante')->references('represent_nome')->on('represent_contatos')->onDelete('CASCADE');
-					$table->foreign('estado')->references('estab_estado')->on('estab_estados')->onDelete('CASCADE');
-					$table->foreign('cidade')->references('estab_cidade')->on('estab_cidades')->onDelete('CASCADE');
-					$table->foreign('endereco')->references('estab_ender')->on('estab_enders')->onDelete('CASCADE');
-					$table->foreign('veiculos')->references('veiculos')->on('veiculos')->onDelete('CASCADE');
-					$table->foreign('cep')->references('estab_cep')->on('estab_ceps')->onDelete('CASCADE');
+					$table->foreign('estabelecimentos')->references('estab_id')->      on('estabelecimentos')->  onDelete('CASCADE');
+          $table->foreign('estab_email')->     references('estab_email')->   on('estab_emails')->      onDelete('CASCADE');
+					$table->foreign('tipo_servico')->    references('tipo_servico')->  on('tipo_servicos')->     onDelete('CASCADE');
+					$table->foreign('telefones')->       references('estab_tel')->     on('telefones')->         onDelete('CASCADE');
+					$table->foreign('representante')->   references('represent_nome')->on('represent_contatos')->onDelete('CASCADE');
+					$table->foreign('estado')->          references('estab_estado')->  on('estab_estados')->     onDelete('CASCADE');
+					$table->foreign('cidade')->          references('estab_cidade')->  on('estab_cidades')->     onDelete('CASCADE');
+          $table->foreign('bairro')->          references('estab_bairro')->  on('estab_bairros')->     onDelete('CASCADE');
+          $table->foreign('endereco')->        references('estab_ender')->   on('estab_enders')->      onDelete('CASCADE');
+					$table->foreign('veiculos')->        references('veiculos')->      on('veiculos')->          onDelete('CASCADE');
+          $table->foreign('cep')->             references('estab_cep')->     on('estab_ceps')->        onDelete('CASCADE');
 					$table->timestamps();
         });
     }
