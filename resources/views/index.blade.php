5fadb2b5844c0fe7dc1f8d6b08a30f22889d04ba
LivSouto <livia.souto@etec.sp.gov.br>
2018-02-06 17:17:36 -0200

    Esqueci de colocar o código junto.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -101,7 +101,7 @@
 							<div class="row">
 								<div class="col s12 m4 l4">
 									<div class="icon-block" class="grow">
-										<h2 class="center brown-text"><img src="Parceiros/etec.jpg" class="parceiros"></h2>
+										<h2 class="center brown-text"><img src="Parceiros/etec.png" class="parceiros"></h2>
 										<h5 class="center">Etec Antonio Devisate</h5>
 										<p class="light" style="text-align: center; padding: 5px;">Escola Técnica Estadual</p>
 									</div>
@@ -115,7 +115,7 @@
 								</div>
 								<div class="col s12 m4 l4">
 									<div class="icon-block">
-										<h2 class="center brown-text"><img src="Parceiros/citec.jpg" class="parceiros"></h2>
+										<h2 class="center brown-text"><img src="Parceiros/citec.png" class="parceiros"></h2>
 										<h5 class="center">Citec Marília</h5>
 										<p class="light" style="text-align: center; padding: 5px;">Centro de Inovação Técnológico de Marília (CITEC - Marília) </p>
 									</div>
