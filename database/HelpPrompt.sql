47acbb22951752ea498f61f3e8aab93d1e9d1713
LeeoSilva <leeosilva0909@gmail.com>
2018-06-02 19:40:42 -0300

    Mais arquivos obsoletos deletados

--- database/HelpPrompt.sql
+++
@@ -1,3 +0,0 @@
-DROP DATABASE IF EXISTS HelpPrompt;
-CREATE DATABASE HelpPrompt;
-
--- resources/views/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	function connection(){
-	    // Conecta e verifica se ocorreu sucesso na conexão do PostGRESQL.
-	    $connection = pg_connect(env('localhost'), env('3306'), env('HelpPrompt'), env('root'), env(''));	
-		if( !$connection ){ die("Error in connection: " . pg_last_error()); }
-    	return $connection;
-    }
-?>
